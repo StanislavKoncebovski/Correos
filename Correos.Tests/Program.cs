e34810b33414bb9d8ceeb90361489259b56ccc16
Stanislav <don@don-arborio.chimgan.net>
2023-10-06 12:16:01 +0200

    main

---
+++ Correos.Tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using Correos;
+
+[Correos]
+class MyTestClass
+{
+	[RequestTarget]
+	public void MyRequest()
+	{
+
+	}
+}
+
+static class Program
+{
+	static void Main()
+	{
+		Correos.Correos.Register(typeof(MyTestClass));
+	}
+}
---
+++ Correos.Tests/TestCorreos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Correos.Tests
+{
+	[TestFixture]
+	public class TestCorreos
+	{
+		[Test]
+		public void Test() 
+		{
+			Console.WriteLine("!!!");
+		}
+	}
+}
